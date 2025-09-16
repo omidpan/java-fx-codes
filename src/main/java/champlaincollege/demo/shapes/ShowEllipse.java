c73ead922ba98d18ecd0d5918bc8c3e022b47803
leopn <omidpnteaching@gmail.com>
2025-09-16 13:30:43 -0400

    change fill color with observable list

--- src/main/java/champlaincollege/demo/shapes/ShowEllipse.java
+++ src/main/java/champlaincollege/demo/shapes/ShowEllipse.java
@@ -27,20 +27,6 @@ public class ShowEllipse extends Application {
 }
 
 class MyEllipse extends Pane {
-
-  private void paint() {
-    getChildren().clear();
-    for (int i = 0; i < 16; i++) {
-      // Create an ellipse and add it to pane
-      Ellipse e1 = new Ellipse(getWidth() / 2, getHeight() / 2, 
-        getWidth() / 2 - 50, getHeight() / 2 - 50);
-      e1.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
-      e1.setFill(Color.WHITE);
-      e1.setRotate(i * 180 / 16);
-      getChildren().add(e1);
-    }
-  }
-
   /**
    * This method inherited from Region
    * Actually Pane <-
@@ -51,10 +37,24 @@ class MyEllipse extends Pane {
     super.setWidth(width);
     paint();
   }
-  
+
   @Override
   public void setHeight(double height) {
     super.setHeight(height);
     paint();
   }
+  private void paint() {
+    getChildren().clear();
+    for (int i = 0; i < 16; i++) {
+      // Create an ellipse and add it to pane
+      Ellipse e1 = new Ellipse(getWidth() / 2, getHeight() / 2, 
+        getWidth() / 2 - 50, getHeight() / 2 - 50);
+      e1.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
+      e1.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+      e1.setRotate(i * 180 / 16);
+      getChildren().add(e1);
+    }
+  }
+
+
 }
