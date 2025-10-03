58d1c2494387f1768ded4480a890f45ad7828853
leopn <omidpnteaching@gmail.com>
2025-10-03 00:14:50 -0400

    case study Tic Tac Toe

--- src/main/java/champlaincollege/demo/chapter16/casestudy/TicTacToe.java
+++ src/main/java/champlaincollege/demo/chapter16/casestudy/TicTacToe.java
@@ -102,31 +102,22 @@ public class TicTacToe extends Application {
       token = c;
       
       if (token == 'X') {
-        Line line1 = new Line(10, 10, 
-          this.getWidth() - 10, this.getHeight() - 10);
+        Line line1 = new Line(10, 10, this.getWidth() - 10, this.getHeight() - 10);
         line1.endXProperty().bind(this.widthProperty().subtract(10));
         line1.endYProperty().bind(this.heightProperty().subtract(10));
-        Line line2 = new Line(10, this.getHeight() - 10, 
-          this.getWidth() - 10, 10);
-        line2.startYProperty().bind(
-          this.heightProperty().subtract(10));
+        Line line2 = new Line(10, this.getHeight() - 10, this.getWidth() - 10, 10);
+        line2.startYProperty().bind(this.heightProperty().subtract(10));
         line2.endXProperty().bind(this.widthProperty().subtract(10));
         
         // Add the lines to the pane
         this.getChildren().addAll(line1, line2); 
       }
       else if (token == 'O') {
-        Ellipse ellipse = new Ellipse(this.getWidth() / 2, 
-          this.getHeight() / 2, this.getWidth() / 2 - 10, 
-          this.getHeight() / 2 - 10);
-        ellipse.centerXProperty().bind(
-          this.widthProperty().divide(2));
-        ellipse.centerYProperty().bind(
-            this.heightProperty().divide(2));
-        ellipse.radiusXProperty().bind(
-            this.widthProperty().divide(2).subtract(10));        
-        ellipse.radiusYProperty().bind(
-            this.heightProperty().divide(2).subtract(10));   
+        Ellipse ellipse = new Ellipse(this.getWidth() / 2, this.getHeight() / 2, this.getWidth() / 2 - 10, this.getHeight() / 2 - 10);
+        ellipse.centerXProperty().bind(this.widthProperty().divide(2));
+        ellipse.centerYProperty().bind(this.heightProperty().divide(2));
+        ellipse.radiusXProperty().bind(this.widthProperty().divide(2).subtract(10));
+        ellipse.radiusYProperty().bind(this.heightProperty().divide(2).subtract(10));
         ellipse.setStroke(Color.BLACK);
         ellipse.setFill(Color.WHITE);
         
