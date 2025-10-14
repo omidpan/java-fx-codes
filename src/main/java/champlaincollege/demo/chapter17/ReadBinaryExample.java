a061d98462034607229ca2b0211e07cafdffc505
leopn <omidpnteaching@gmail.com>
2025-10-14 09:53:16 -0400

    Chapter 17 init

---
+++ src/main/java/champlaincollege/demo/chapter17/ReadBinaryExample.java
@@ -0,0 +1,22 @@
+package champlaincollege.demo.chapter17;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class ReadBinaryExample {
+
+    public static void main(String[] args) {
+        byte[]bytes=new byte[5];
+        bytes="HELLO".getBytes();
+
+        for (int i=0;i<bytes.length;i++){
+            System.out.print(bytes[i]+"\t");
+            System.out.print(new String(new byte[]{bytes[i]}));
+            System.out.println();
+        }
+
+
+
+
+    }
+}
