fa68eec1ede2572550a7f5fc0ff616501e3c971b
leopn <omidpnteaching@gmail.com>
2025-10-17 08:03:36 -0400

    add TestFileStream

---
+++ src/main/java/champlaincollege/demo/chapter17/TestFileStream.java
@@ -0,0 +1,25 @@
+package champlaincollege.demo.chapter17;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TestFileStream {
+  public static void main(String[] args) throws IOException {
+    // Create an output stream to the file
+    try (FileOutputStream output = new FileOutputStream("temp.dat")) {
+      // Output values to the file
+      for (int i = 1; i <= 10; i++)
+        output.write(i);
+    }
+    System.out.println("Output Writing finished.");
+    System.out.println("Read the file from temp.dat");
+    // Create an input stream for the file
+    try ( FileInputStream input = new FileInputStream("temp.dat")) {
+      // Read values from the file
+      int value;
+      while ((value = input.read()) != -1)
+        System.out.print(value + " ");
+    }
+  }
+}
